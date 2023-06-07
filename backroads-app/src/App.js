6eee37a07ec759a68870620a12ce5741d4701dd5
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-07 23:47:18 +0100

    splitted the components

--- backroads-app/src/App.js
+++ backroads-app/src/App.js
@@ -1,7 +1,20 @@
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Tours from './components/Tours';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <h1>Backroads app</h1>
+    <>
+      <NavBar/>
+      <Hero/>
+      <About/>
+      <Services/>
+      <Tours/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ backroads-app/src/components/NavBar.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import logo from '../images/logo.svg'
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        <ul className="nav-links" id="nav-links">
+          <li>
+            <a href="#home" className="nav-link"> home </a>
+          </li>
+
+          <li>
+            <a href="#about" className="nav-link"> about </a>
+          </li>
+
+          <li>
+            <a href="#services" className="nav-link"> services </a>
+          </li>
+
+          <li>
+            <a href="#tours" className="nav-link"> tours</a>
+          </li>
+        </ul>
+
+        <ul className="nav-icons">
+          <li>
+            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
+              ><i className="fab fa-facebook"></i
+            ></a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
+              ><i className="fab fa-twitter"></i
+            ></a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
+              ><i className="fab fa-squarespace"></i
+            ></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
