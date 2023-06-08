aba8a43585e102544a61c10fa1c012e3c699ab26
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-08 15:01:33 +0100

    updated services

--- backroads-app/src/components/Services.js
+++ backroads-app/src/components/Services.js
@@ -1,43 +1,23 @@
 import React from 'react'
 import Title from './Title'
+import { services } from '../data'
 
 const Services = () => {
   return (
     <section className="section services" id="services">
       <Title firstWord="our" secondWord="services" />
       <div className="section-center services-center">
-        <article className="service">
-          <span className="service-icon"><i className="fas fa-wallet fa-fw"></i></span>
+        {services.map((service) => (
+          <article className="service" key={service.id}>
+          <span className="service-icon"><i className={service.icon}></i></span>
           <div className="service-info">
-            <h4 className="service-title">saving money</h4>
+            <h4 className="service-title">{service.title}</h4>
             <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
+              {service.text}
             </p>
           </div>
         </article>
-
-        <article className="service">
-          <span className="service-icon"><i className="fas fa-tree fa-fw"></i></span>
-          <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon"><i className="fas fa-socks fa-fw"></i></span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
+        ))}
       </div>
     </section>
   )
--- backroads-app/src/data.js
+++ backroads-app/src/data.js
@@ -38,3 +38,25 @@ export const socialLinks = [
         icon: "fa-squarespace"
     }
 ]
+
+export const services = [
+    {
+        id: 1,
+        title: "saving money",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+        icon: "fas fa-wallet fa-fw"
+    },
+    {
+        id: 2,
+        title: "endless hiking",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+        icon: "fas fa-tree fa-fw"
+    },
+    {
+        id: 3,
+        title: "amazing comfort",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+        icon: "fas fa-socks fa-fw"
+    }
+]
+
