64da1898c478c94be4489718435f930a6764131a
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-11 23:58:28 +0100

    completed user challenge and conditional rendering tutorial

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/starter/06-toggle-challenge'
+import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge'
 
 function App() {
   return (
--- react-hooks/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ react-hooks/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null)
+  const handleLoginBtn = () => {
+    setUser({name: "ahmed"})
+  }
+  const handleLogoutBtn = () => {
+    setUser(null)
+  }
+  return <div className="container">
+    <h2>User Challenge</h2>
+    {user ? <h2>Welcome {user.name}</h2> : <h2>Please Login</h2>}
+    {user ? <button className="btn" onClick={handleLogoutBtn} >Logout</button> : <button className="btn" onClick={handleLoginBtn} >Login</button>}
+  </div>;
 };
 
 export default UserChallenge;
