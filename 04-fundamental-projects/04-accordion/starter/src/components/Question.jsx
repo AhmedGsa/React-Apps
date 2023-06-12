198686fae3b802c3e494d778ffe5d32922a360e7
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-12 14:37:26 +0100

    completed extra challenge

--- 04-fundamental-projects/04-accordion/starter/src/components/Question.jsx
+++ 04-fundamental-projects/04-accordion/starter/src/components/Question.jsx
@@ -3,18 +3,18 @@ import { useState } from 'react'
 import {AiOutlinePlus, AiOutlineMinus} from 'react-icons/ai'
 
 const Question = (props) => {
-    const {title, info} = props
-    const [displayInfo, setDisplayInfo] = useState(false)
-    const toggleInfo = () => {
-        setDisplayInfo(!displayInfo)
-    }
+    const {title, info, toggleQst, activeQst, id} = props
+    // const [displayInfo, setDisplayInfo] = useState(false)
+    // const toggleInfo = () => {
+    //     setDisplayInfo(!displayInfo)
+    // }
   return (
     <div className='question'>
         <header>
             <h5>{title}</h5>
-            <button className='question-btn' onClick={toggleInfo}>{displayInfo ? <AiOutlineMinus/> : <AiOutlinePlus/>}</button>
+            <button className='question-btn' onClick={() => {toggleQst(id)}}>{activeQst === id ? <AiOutlineMinus/> : <AiOutlinePlus/>}</button>
         </header>
-        {displayInfo && <p>{info}</p>}
+        {activeQst === id && <p>{info}</p>}
     </div>
   )
 }
--- 04-fundamental-projects/04-accordion/starter/src/components/Questions.jsx
+++ 04-fundamental-projects/04-accordion/starter/src/components/Questions.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import data from '../data'
 import Question from './Question'
+import { useState } from 'react'
 
 const Questions = () => {
+    const [activeQst, setActiveQst] = useState(null)
+    const toggleQst = (id) => {
+        setActiveQst(id);
+    }
   return (
     <>
         {data.map((qst) => (
-            <Question {...qst} key={qst.id} />
+            <Question {...qst} key={qst.id} activeQst={activeQst} toggleQst={toggleQst} />
         ))}
     </>
   )
