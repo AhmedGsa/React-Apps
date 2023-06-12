b15e3bb0ea09fdd8771b01bb9df4e567e0e1b707
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-12 02:02:31 +0100

    completed show more btn and tours project

--- 04-fundamental-projects/02-tours/starter/src/components/Tour.jsx
+++ 04-fundamental-projects/02-tours/starter/src/components/Tour.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Tour = (props) => {
+    const [showMore, setShowMore] = useState(false)
     const {name, info, image, price, removeTour, id} = props;
-    // const handleDeleteBtn = () => {
-    //   setShowed(false)
-    // }
+    const handleShowMoreBtn = () => {
+      setShowMore(true)
+    }
   return (
     <div className='single-tour'>
         <img src={image} style={{width: "300px", height: "300px", objectFit: "cover"}}></img>
         <span className='tour-price'>{`$${price}`}</span>
         <div className="tour-info">
             <h5 className='title'>{name}</h5>
-            <p>{info}</p>
+            {showMore ? <p>{info}</p> : <p>{info.substring(0, 100)}... <span className='info-btn' onClick={handleShowMoreBtn}>Read more</span></p>}
             <button className='btn delete-btn' onClick={() => {removeTour(id)}}>Not interested</button>
         </div>
     </div>
