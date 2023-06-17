911bb9529ee2db0dde658d2dad18e9231f12e71e
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-17 14:50:34 +0100

    completed user challenge

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/06-forms/starter/01-controlled-inputs'
+import Starter from './tutorial/06-forms/starter/02-user-challenge'
 
 function App() {
   return (
--- react-hooks/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ react-hooks/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,47 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UserChallenge = () => {
+  const [name, setName] = useState("");
+  const [users, setUsers] = useState(data);
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUsers([...users, { id: users.length + 1, name }]);
+    setName("");
+  };
+  const removeUser = (id) => {
+    setUsers(users.filter(user => user.id !== id))
+  }
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            onChange={handleNameChange}
+            value={name}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
-      {/* render users below */}
+      {users.map((user) => (
+        <div key={user.id} style={{display: "flex", alignItems:"center", justifyContent: "center", gap: "25px"}}>
+          <h4 key={user.id}>{user.name}</h4>
+          <button className="btn" onClick={() => {removeUser(user.id)} }>Remove</button>
+        </div>
+      ))}
     </div>
   );
 };
