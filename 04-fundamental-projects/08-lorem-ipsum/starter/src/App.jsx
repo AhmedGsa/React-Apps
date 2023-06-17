999897b5b06e5bcfeec8d300631d1b828d9df942
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-17 19:02:40 +0100

    completed lorem ipsum generator project

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+import data from './data'
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1)
+  const [generatedText, setGeneratedText] = useState([])
+  const handleInputChange = (e) => {
+    setCount(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setGeneratedText(data.slice(0,count))
+  }
+  return <div className="section-center">
+    <h4>TIRED OF BORING LOREM IPSUM?</h4>
+    <form className="lorem-form" onSubmit={handleSubmit}>
+      <label htmlFor="count">Paragraphs:</label>
+      <input type="number" id="count" value={count} min={1} max={8} onChange={handleInputChange}></input>
+      <button type="submit" className="btn" >Generate</button>
+    </form>
+    <div className="lorem-text">
+      {generatedText.map((text) => <p>{text}</p>)}
+    </div>
+  </div>;
 };
 export default App;
