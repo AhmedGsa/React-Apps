df8775188d32d334cda13c0c703b61ffbd389070
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-17 14:33:42 +0100

    completed controllers inputs tutorial

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,10 +1,9 @@
-import Starter from './tutorial/05-leverage-javascript/starter/List'
-import { people } from './data';
+import Starter from './tutorial/06-forms/starter/01-controlled-inputs'
 
 function App() {
   return (
     <div className='container'>
-      <Starter people={people}/>
+      <Starter />
     </div>
   );
 }
--- react-hooks/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ react-hooks/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,47 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, name);
+  }
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <h4>controlled inputs</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          id="name"
+          onChange={handleNameChange}
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          className="form-input"
+          id="email"
+          onChange={handleEmailChange}
+        />
+      </div>
+      <button type="submit" className="btn btn-block">
+        submit
+      </button>
+    </form>
+  );
 };
 export default ControlledInputs;
