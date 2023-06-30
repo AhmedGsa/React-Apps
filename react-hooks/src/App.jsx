7d320089e8c9b01dbd1326a1702baac65b8fa98b
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-30 18:24:11 +0100

    completed useTransition tutorial

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/11-performance/starter/03-hooks'
+import Starter from './tutorial/11-performance/starter/04-react-18'
 
 function App() {
   return (
--- react-hooks/src/tutorial/11-performance/starter/04-react-18/index.jsx
+++ react-hooks/src/tutorial/11-performance/starter/04-react-18/index.jsx
@@ -1,27 +1,30 @@
-import { useState } from 'react';
+import { useState, useTransition } from "react";
 const LatestReact = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [items, setItems] = useState([]);
+  const [isPending, startTransition] = useTransition();
 
   const handleChange = (e) => {
     setText(e.target.value);
 
     // slow down CPU
-    // const newItems = Array.from({ length: 5000 }, (_, index) => {
-    //   return (
-    //     <div key={index}>
-    //       <img src='/vite.svg' alt='' />
-    //     </div>
-    //   );
-    // });
-    // setItems(newItems);
+    startTransition(() => {
+      const newItems = Array.from({ length: 5000 }, (_, index) => {
+        return (
+          <div key={index}>
+            <img src="/vite.svg" alt="" />
+          </div>
+        );
+      });
+      setItems(newItems);
+    });
   };
   return (
     <section>
-      <form className='form'>
+      <form className="form">
         <input
-          type='text'
-          className='form-input'
+          type="text"
+          className="form-input"
           value={text}
           onChange={handleChange}
         />
@@ -30,12 +33,12 @@ const LatestReact = () => {
 
       <div
         style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr 1fr 1fr',
-          marginTop: '2rem',
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr",
+          marginTop: "2rem",
         }}
       >
-        {items}
+        {isPending ? <h4>Loading ...</h4> : items}
       </div>
     </section>
   );
