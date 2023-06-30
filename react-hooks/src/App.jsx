bc09d483ad86d3035938d2af4bbf6bce75c0aa4b
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-30 17:24:53 +0100

    completed lower state challenge

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/10-useReducer/starter/01-useReducer'
+import Starter from './tutorial/11-performance/starter/02-lower-state-challenge'
 
 function App() {
   return (
--- react-hooks/src/tutorial/11-performance/starter/02-lower-state-challenge/index.jsx
+++ react-hooks/src/tutorial/11-performance/starter/02-lower-state-challenge/index.jsx
@@ -1,44 +1,18 @@
 import { useState } from 'react';
 import { data } from '../../../../data';
 import List from './List';
+import Form from './Form';
 const LowerStateChallenge = () => {
   const [people, setPeople] = useState(data);
-  const [name, setName] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!name) {
-      alert('Please Provide Name Value');
-      return;
-    }
-    addPerson();
-    setName('');
-  };
-  const addPerson = () => {
+  
+  const addPerson = (name) => {
     const fakeId = Date.now();
     const newPerson = { id: fakeId, name };
     setPeople([...people, newPerson]);
   };
   return (
     <section>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input
-            type='text'
-            name='name'
-            id='name'
-            className='form-input'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <button className='btn btn-block' type='submit'>
-          submit
-        </button>
-      </form>
+      <Form addPerson={addPerson} />
       <List people={people} />
     </section>
   );
