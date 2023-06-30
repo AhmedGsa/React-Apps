594d9c69d70d16ad676c3d8e6c2c6ff3e5244a09
SilverBullet19 <a_gouasmia@estin.dz>
2023-06-30 18:07:16 +0100

    completed useMemo tutorial

--- react-hooks/src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ react-hooks/src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,9 +1,19 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useMemo } from 'react';
 import { data } from '../../../../data';
 import List from './List';
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
+
+  const slowFunc = () => {
+    let value=0;
+    for(let i=0; i<1000000000; i++) {
+      value++;
+    }
+    return value;
+  }
+  const value = useMemo(() => slowFunc(), []);
+  console.log(value);
 
   const removePerson = useCallback((id) => {
     const newPeople = people.filter((person) => person.id !== id)
