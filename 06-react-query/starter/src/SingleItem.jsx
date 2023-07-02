ced72d51e3bb8c2bdbf7960c401139944999856a
SilverBullet19 <a_gouasmia@estin.dz>
2023-07-02 20:53:07 +0100

    completed update and delete task

--- 06-react-query/starter/src/SingleItem.jsx
+++ 06-react-query/starter/src/SingleItem.jsx
@@ -1,23 +1,48 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import customAxios from "./utils";
+import { toast } from "react-toastify";
+
 const SingleItem = ({ item }) => {
+  const queryClient = useQueryClient()
+  const { mutate: updateTask } = useMutation({
+    mutationFn: ({id, isDone}) => customAxios.patch(`/${id}`, { isDone: isDone }),
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    },
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({queryKey: ["tasks"]})
+    },
+  });
+  const { mutate: deleteTask } = useMutation({
+    mutationFn: ({id}) => customAxios.delete(`/${id}`),
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    },
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({queryKey: ["tasks"]})
+    },
+  });
   return (
-    <div className='single-item'>
+    <div className="single-item">
       <input
-        type='checkbox'
+        type="checkbox"
         checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        onChange={(e) => {
+          updateTask({id: item.id, isDone: e.target.checked});
+        }}
       />
       <p
         style={{
-          textTransform: 'capitalize',
-          textDecoration: item.isDone && 'line-through',
+          textTransform: "capitalize",
+          textDecoration: item.isDone && "line-through",
         }}
       >
         {item.title}
       </p>
       <button
-        className='btn remove-btn'
-        type='button'
-        onClick={() => console.log('delete task')}
+        className="btn remove-btn"
+        type="button"
+        onClick={() => deleteTask({id: item.id})}
       >
         delete
       </button>
